102c36215d9a48e287c1a3fce9a35e849210de83
tail <tail001@126.com>
2022-11-10 10:29:37 +0800

    spricloud

---
+++ 01-springcloud-provide/src/main/java/com/tail/springcloud/controller/ProviderController.java
@@ -0,0 +1,19 @@
+package com.tail.springcloud.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 作者:admin
+ * 时间:2022/11/8
+ * 版本:
+ * 功能:com.tail.springcloud.controller 生产者提供json
+ * Web后端springcloud
+ */
+@RestController
+public class ProviderController {
+    @RequestMapping("/test")
+    public Object test(){
+        return "springcloud第一个生产者";
+    }
+}
